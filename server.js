5f36335c7b2878b30b6891f244f6165e63d2af24
Adriano-97 <adriano199726@gmail.com>
2019-11-22 13:38:33 -0500

    Added  middlware support and a gloval variable to hold the root.

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-const SwaggerExpress = require('swagger-express-mw');
-const app = require('express')();
-const connectDB = require('./config/db');
+//Declaring a gloval variable For the app root.
+global.__rootdir = __dirname;
 
-const config = {
-  appRoot: __dirname
-};
+const express = require('express');
+const swagger_express = require('swagger-express-mw');
+const app = express();
+const connectDB = require('./config/db');
+const swaggerConfig = require('./config/swaggerCon');
+
+//Conect to the DB
 connectDB();
 
-SwaggerExpress.create(config, function(err, swaggerExpress) {
+//Init Middleware
+app.use(express.json());
+
+swagger_express.create(swaggerConfig, function(err, swaggerExpress) {
   if (err) {
     throw err;
   }
