af298272e107a4f3a4969b31e284b109b5976ef9
Adriano-97 <adriano199726@gmail.com>
2019-12-23 23:10:11 -0500

    Added some test routes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//Declarations.
 const express = require('express');
 const connectDB = require('./config/db');
 
@@ -8,6 +9,14 @@ const app = express();
 connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+//Set port for production or dev.
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
