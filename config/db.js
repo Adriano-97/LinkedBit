2a6b51e709f4a0957baf80e86755f8a02f5916a6
Adriano-97 <adriano199726@gmail.com>
2019-12-23 22:54:37 -0500

    This file Connencts to the DB.

---
+++ config/db.js
@@ -0,0 +1,27 @@
+//Declarations
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+/*
+  This Async method will connect to the database using mongoose.
+If the connection fails it will exit the connection with one plus will
+dysplay the error log in the console.
+
+  #db info is pulled from the default.jason file
+*/
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    Console.error(err.message);
+
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
