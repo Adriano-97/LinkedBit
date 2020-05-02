8e37da0f1eef4c02640fa00c7996b0e4f4a85cbd
Adriano-97 <adriano199726@gmail.com>
2020-05-02 10:33:18 -0400

    Added the PUT request for api/profiles/experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -157,4 +157,56 @@ router.delete('/', auth, async (req, res) => {
   }
 });
 
+// @route PUT api/profile/experience
+// @desc Add profile experience
+// @acces Private
+router.put(
+  '/experience',
+  [
+    auth,
+    check('title', 'Title is Required').not().isEmpty(),
+    check('company', 'Company is Required').not().isEmpty(),
+    check('from', 'From Date is Required').not().isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      descryption,
+    } = req.body;
+
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      descryption,
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.experience.unshift(newExp);
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
 module.exports = router;
