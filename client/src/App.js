cb5b3259d077a1df91e834ba5af372c2b1f0c3f8
Adriano-97 <adriano199726@gmail.com>
2020-05-03 07:14:24 -0400

    Created a basic template for the react application

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './commponents/layout/Navbar';
+import Landing from './commponents/layout/Landing';
+import Login from './commponents/auth/Login';
+import Register from './commponents/auth/Register';
+import './App.css';
+
+const App = () => (
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path='/' component={Landing} />
+      <section className='container'>
+        <Switch>
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
+);
+export default App;
